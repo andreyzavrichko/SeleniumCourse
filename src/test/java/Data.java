bc7b982be0f475a71a192f16fe5502709057482b
Andrey Zavrichko <andrey@zavrichko.dev>
2021-11-28 10:57:41 +0200

    add test for registration

---
+++ src/test/java/Data.java
@@ -0,0 +1,10 @@
+public class Data {
+
+    public String getUrl = "https://unsplash.com/";
+    public String firstName = "Andrey654";
+    public String lastName = "Petrov321";
+    public String email = "petrov34564@gmail.com";
+    public String userName = "petrov3214554";
+    public String password = "13456789";
+
+}
