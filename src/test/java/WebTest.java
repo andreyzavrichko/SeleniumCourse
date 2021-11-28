49a694b9227601f98cdbb9a1f5a034e65af58ff1
Andrey Zavrichko <andrey@zavrichko.dev>
2021-11-28 09:51:31 +0200

    first autotest

---
+++ src/test/java/WebTest.java
@@ -0,0 +1,10 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebTest {
+
+    public static void main(String[] args) {
+        WebDriver auto = new ChromeDriver();
+        auto.get("https://unsplash.com/");
+    }
+}
